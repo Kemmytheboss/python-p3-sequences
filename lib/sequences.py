6fe7658e596bbe82297b62cc5cb511aad507e588
Verah Mokaya <verahmokaya475@gmail.com>
2025-11-18 23:13:08 +0300

    assigment done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    fib = [0, 1]
+    while len(fib) < length:
+        next_number = fib[-1] + fib[-2]
+        fib.append(next_number)
+
+    print(fib)
+
+
+# Example usage:
+if __name__ == "__main__":
+    print_fibonacci(9)
